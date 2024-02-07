827615620a38b4a5c2c8146132088ec763eb43ec
Ruthishkumarr <ruthishkumarrajendran@gmail.com>
2024-02-07 19:09:57 +0530

    Updated

---
+++ get.js
@@ -0,0 +1,7 @@
+// const span = document.createElement("span");
+// span.innerText = "Hi How Are You";
+// span.setAttribute("class", "s");
+// document.body.append(span);
+
+const res3 = document.querySelectorAll(".su");
+console.log(res3);
